81ec56e585aac226fe2da21d43d8ad50a89744e7
TatianaSMitsuk <fier99@mail.ru>
2025-11-30 01:27:19 +0500

    tenth commit

--- function_load.py
+++ function_load.py
@@ -21,6 +21,18 @@ def loading_file(token):
         with open(const_test_load.file_name, 'rb') as f:
             requests.put(dynamic_url, f)
 
+def loading_file_with_param(file_name, token):
+    headers = {
+                'Authorization': f'OAuth {token}',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+    url_to_file = f'{const_test_load.const_url}/upload?path={const_test_load.dir_path}/{file_name}'
+    response_get = requests.get(url_to_file, headers=headers)
+    if response_get.status_code == 200:
+        dynamic_url = response_get.json()["href"]
+        with open(file_name, 'rb') as f:
+            requests.put(dynamic_url, f)
+
 
 def direct_del(token):
     headers = {'Authorization': f'OAuth {token}'}
@@ -30,15 +42,15 @@ def direct_del(token):
     requests.delete(const_test_load.const_url, headers=headers, params=params)
 
 
-def get_md5_for_file(file_path):
+def get_md5_for_file(file_name):
     md5_hash = hashlib.md5()
-    with open(file_path, "rb") as f:
+    with open(file_name, "rb") as f:
          for byte_block in iter(lambda: f.read(4096), b""):
             md5_hash.update(byte_block)
     return md5_hash.hexdigest()
 
 
-def get_md5_file_yadisk(file_path, token):
+def get_md5_file_yadisk(direct_name, token):
     headers = {'Authorization': f'OAuth {token}'}
-    response=requests.get(file_path,headers=headers)
+    response=requests.get(direct_name,headers=headers)
     return response.json()['_embedded']['items'][0]['md5']
--- test_load_file_with_fixt_param_const.py
+++ test_load_file_with_fixt_param_const.py
@@ -10,19 +10,18 @@ dotenv.load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
 
-@pytest.fixture(params=["1.mp3",'example.txt'])
-def fixt_file_load(request):
-    const_test_load.file_name=request.param
-    function_load.direct_create(TOKEN)
-    function_load.loading_file(TOKEN)
+@pytest.fixture
+def fixt_file_load():
+    function_load.direct_create( TOKEN)
 
     yield
 
     function_load.direct_del(TOKEN)
     time.sleep(2)
 
-
-def test_load_file (fixt_file_load):
-    md5_file_in = function_load.get_md5_for_file(const_test_load.file_name)
+@pytest.mark.parametrize('file_name',[const_test_load.file_name,const_test_load.file_name_second])
+def test_load_file (fixt_file_load, file_name):
+    function_load.loading_file_with_param(file_name, TOKEN)
+    md5_file_in = function_load.get_md5_for_file(file_name)
     url = f'{const_test_load.const_url}?path={const_test_load.dir_path}'
     assert  str(md5_file_in) == str(function_load.get_md5_file_yadisk(url, TOKEN))
