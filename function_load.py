2e1e55f65691d9ba8d3ac964c0a3671f2bb95232
TatianaSMitsuk <fier99@mail.ru>
2025-12-02 21:42:00 +0500

    dog_first

--- function_load.py
+++ function_load.py
@@ -20,6 +20,7 @@ def loading_file_with_param(file_name, token):
         dynamic_url = response_get.json()["href"]
         with open(file_name, 'rb') as f:
             requests.put(dynamic_url, f)
+            f.close()
 
 
 def direct_del(token):
@@ -28,6 +29,7 @@ def direct_del(token):
         'path': const_test_load.dir_path
     }
     requests.delete(const_test_load.const_url, headers=headers, params=params)
+    print('Убрали за собой')
 
 
 def get_md5_for_file(file_name):
@@ -38,7 +40,52 @@ def get_md5_for_file(file_name):
     return md5_hash.hexdigest()
 
 
-def get_md5_file_yadisk(direct_name, token):
+def get_md5_file_yadisk(direct_name, file_name, token):
     headers = {'Authorization': f'OAuth {token}'}
     response=requests.get(direct_name,headers=headers)
-    return response.json()['_embedded']['items'][0]['md5']
+    for file_on_ydisk in response.json()['_embedded']['items']:
+        if file_on_ydisk['name']==file_name:
+            md_five=file_on_ydisk['md5']
+    return md_five
+
+def get_list_of_breed():
+    response=requests.get(const_test_load.url_breed+'s/list/all')
+    breed=[]
+    breed_with_subb=[]
+    for key,value in response.json()['message'].items():
+        if len(value)==0:
+            breed.append(key)
+        else:
+            breed_with_subb.append(key)
+    return breed, breed_with_subb
+
+
+def get_three_foto_of_breed(breed):
+    url_3_foto=const_test_load.url_breed+'/'+breed+'/images/random/3'
+    response = requests.get(url_3_foto)
+    list_foto_name=response.json()['message']
+    return list_foto_name
+
+def get_foto(url):
+    dog_foto_name = url.split('/')[-1]
+    print(url)
+    print('Зашел', dog_foto_name)
+    dog_foto = requests.get(url)
+    print(dog_foto)
+    print('1')
+    with open(dog_foto_name, 'wb') as file:
+        file.write(dog_foto.content)
+        print('2')
+    return dog_foto_name
+
+
+def get_subbread(breed):
+    url_sbb=const_test_load.url_breed+'/'+breed+'/list'
+    sbb = requests.get(url_sbb)
+    return sbb.json()['message']
+
+
+def get_url_rand_foto_subbread(breed, sbb):
+    url=const_test_load.url_breed+'/'+breed+'/'+sbb+'/images/random'
+    url_file=requests.get(url)
+    return url_file.json()['message']
