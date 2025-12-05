34ca75e5b410d303f2d1cca228808c9b8cf33a5f
TatianaSMitsuk <fier99@mail.ru>
2025-12-05 19:31:42 +0500

    cat_second

--- test_cats_api_with_tekst_to_yadisk.py
+++ test_cats_api_with_tekst_to_yadisk.py
@@ -2,7 +2,7 @@ import dotenv
 import os
 
 import const_for_cats
-import function_cat_api_with_text
+import cat_api
 import function_work_witn_api_yadisk as f
 import const_test_load as cnst
 import pytest
@@ -20,11 +20,10 @@ def fixt_cat_api_to_yadisk():
     f.direct_del(TOKEN)
 
 
-@pytest.mark.parametrize('text',const_for_cats.hardkod)
+@pytest.mark.parametrize('text',const_for_cats.title)
 def test_load_file (fixt_cat_api_to_yadisk,text):
 
-    cat_foto=function_cat_api_with_text.get_random_photo(f'{const_for_cats.cats_url}/says/{const_for_cats.hardkod[0]}?json=true')
+    cat_foto=cat_api.get_random_photo(f'{const_for_cats.cats_url}/says/{text}?json=true')
     f.loading_file_with_param(cat_foto,TOKEN)
     assert f.get_md5_for_file(cat_foto)==f.get_md5_file_yadisk(cnst.dir_path,cat_foto,TOKEN)
-    print(cat_foto)
     os.remove(cat_foto)
