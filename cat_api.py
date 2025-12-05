c8d79b11a170dc8bd4df04e205c8e1f1a1a2d523
TatianaSMitsuk <fier99@mail.ru>
2025-12-05 19:32:39 +0500

    cat_second

--- cat_api.py
+++ cat_api.py
@@ -4,12 +4,9 @@ import random
 
 def get_random_photo(url):
     cat_photo = requests.get(url)
-    print(cat_photo.json())
     type_foto=cat_photo.json()['mimetype'].split('/')[-1]
     cat_photo_name = f'cat{random.randint(0, 1000)}.{type_foto}'
-    print(cat_photo_name)
     cat_photo=requests.get(cat_photo.json()['url'])
-    print('1')
     with open(cat_photo_name, 'wb') as file:
         file.write(cat_photo.content)
     return cat_photo_name
